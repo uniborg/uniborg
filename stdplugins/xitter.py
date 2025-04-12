792659b99868360777851a99ca4d7694cb6fbcf0
Qwerty-Space <oliver_duce@live.co.uk>
2025-04-12 21:09:16 +0100

    add xitter plugin

---
+++ stdplugins/xitter.py
@@ -0,0 +1,13 @@
+"""
+*Xitter*
+Replaces twitter links with fixupx links
+"""
+import re
+from telethon import events
+
+@borg.on(events.NewMessage(outgoing=True, pattern=re.compile(r"(twitter|x)\.com(?=\/\S+)").search))
+async def xitter(event):
+    m = event.pattern_match.group(1)
+    new_text = (event.raw_text).replace(m, "fixupx")
+    await event.edit(new_text)
+
