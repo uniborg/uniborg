bad68d6d55fde729f2138d90f59bd41cd2909b77
Qwerty-Space <oliver_duce@live.co.uk>
2025-04-24 15:25:16 +0100

    Better regex + link preview

--- stdplugins/xitter.py
+++ stdplugins/xitter.py
@@ -5,9 +5,9 @@ Replaces twitter links with fixupx links
 import re
 from telethon import events
 
-@borg.on(events.NewMessage(outgoing=True, pattern=re.compile(r"(twitter|x)\.com(?=\/\S+)").search))
+@borg.on(events.NewMessage(outgoing=True, pattern=re.compile(r"\b(twitter|x)\.com(?=\/\S+)").search))
 async def xitter(event):
     m = event.pattern_match.group(1)
     new_text = (event.raw_text).replace(m, "fixupx")
-    await event.edit(new_text)
+    await event.edit(new_text, link_preview=True)
 
