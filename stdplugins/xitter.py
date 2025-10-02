9a61ad296adda5e3d1a26ec5a3fc9c302e2ad6c0
novenary <novenary@kwak.zip>
2025-10-02 11:46:28 +0000

    xitter: support fixing links mid-message

--- stdplugins/xitter.py
+++ stdplugins/xitter.py
@@ -6,9 +6,10 @@ import re
 from telethon import events
 
 @borg.on(events.NewMessage(outgoing=True,
-    pattern=r"^(?:https?://)?(?:www\.)?(?:x|twitter)\.com/(?P<path>[a-zA-Z0-9_]{4,15}/status/\d+)(?:\?\S*)?$"))
+    pattern=re.compile(r"\b(?:https?://)?(?:www\.)?(?:x|twitter)\.com/(?P<path>[a-zA-Z0-9_]{4,15}/status/\d+)(?:\?\S*)?\b").search))
 async def xitter(event):
+    m = event.pattern_match.group(0)
     path = event.pattern_match.group(1)
-    new_text = f"https://fxtwitter.com/{path}"
+    new_text = (event.raw_text).replace(m, f"https://fxtwitter.com/{path}")
     await event.edit(new_text, link_preview=True)
 
