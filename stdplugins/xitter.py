47300e545d73cc33084e8fbe8bd1b7e1c5e67d76
novenary <novenary@kwak.zip>
2025-10-02 11:39:15 +0000

    xitter: make http(s) optional

--- stdplugins/xitter.py
+++ stdplugins/xitter.py
@@ -6,7 +6,7 @@ import re
 from telethon import events
 
 @borg.on(events.NewMessage(outgoing=True,
-    pattern=r"^https://(?:www\.)?(?:x|twitter)\.com/(?P<path>[a-zA-Z0-9_]{4,15}/status/\d+)(?:\?\S*)?$"))
+    pattern=r"^(?:https?://)?(?:www\.)?(?:x|twitter)\.com/(?P<path>[a-zA-Z0-9_]{4,15}/status/\d+)(?:\?\S*)?$"))
 async def xitter(event):
     path = event.pattern_match.group(1)
     new_text = f"https://fxtwitter.com/{path}"
