ab9317db291038ff69b9d4f9e6f291cbca9b1bb5
novenary <novenary@kwak.zip>
2026-01-19 23:00:26 +0000

    xitter: fix curly braces

--- stdplugins/xitter.py
+++ stdplugins/xitter.py
@@ -23,7 +23,7 @@ domains = "|".join([
 ])
 
 @borg.on(events.NewMessage(outgoing=True,
-    pattern=re.compile(fr"\b(?:https?://)?(?:www\.)?(?:{domains})\.com/(?P<path>(?:i|[a-zA-Z0-9_]{4,15})/status/\d+)(?:\?\S*)?\b").search))
+    pattern=re.compile(fr"\b(?:https?://)?(?:www\.)?(?:{domains})\.com/(?P<path>(?:i|[a-zA-Z0-9_]{{4,15}})/status/\d+)(?:\?\S*)?\b").search))
 async def xitter(event):
     m = event.pattern_match.group(0)
     path = event.pattern_match.group(1)
